84e06ae70fe2e37ce8d912500ea903ce18de49d4
tashabu1126 <tashabu1126@gmail.com>
2023-07-03 23:19:55 +0900

    8-5 ブログ投稿編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,4 +29,16 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post']; //['post']を指定する事でname属性にpostをつけている値を配列として取得し、$input_postという変数に格納している
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
