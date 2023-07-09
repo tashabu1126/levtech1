9822c4ca958d09667d16a9733ee3907692f07165
tashabu1126 <tashabu1126@gmail.com>
2023-07-09 15:39:07 +0900

    カリキュラム08-6. Laravel9基礎：ブログ投稿の削除 完了

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']); //{}はIDの値、editメソッドを引っ張ているs
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
